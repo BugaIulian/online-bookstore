54cb3e4554cfdef18ef061efc0650bfda4dc8646
pc <andreigabriel1005@gmail.com>
2023-04-23 17:24:13 +0300

    First commit

---
+++ online-bookstore/src/test/java/group1/itschool/onlinebookstore/OnlineBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package group1.itschool.onlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
