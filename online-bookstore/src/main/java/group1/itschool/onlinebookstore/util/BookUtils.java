7898defa0466fc54e678bd801f214751fe9bd893
Iulian <iulian.ionut.buga@gmail.com>
2023-04-24 12:49:46 +0300

    Add book genre constants in utils package
    - create a new package labeled util
    - create a new class labeled BookUtils.java
    - final constants have been added for books genre

---
+++ online-bookstore/src/main/java/group1/itschool/onlinebookstore/util/BookUtils.java
@@ -0,0 +1,16 @@
+package group1.itschool.onlinebookstore.util;
+
+public class BookUtils {
+
+    /** Constants of books genre that can be used later in the source code. This comment will be erased once done. */
+
+    public static final String GENRE_HORROR = "Horror";
+    public static final String GENRE_MYSTERY = "Mystery";
+    public static final String GENRE_DRAMA = "Drama";
+    public static final String GENRE_POETRY = "Poetry";
+    public static final String GENRE_ACTION = "Action";
+    public static final String GENRE_FICTION = "Fiction";
+    public static final String GENRE_SATIRE = "Satire";
+    public static final String GENRE_HISTORICAL = "Historical";
+    public static final String GENRE_PARODY = "Parody";
+}
