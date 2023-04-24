1a674bc6ad1579673df8ce5a95435b7391e8c2fc
Iulian <iulian.ionut.buga@gmail.com>
2023-04-24 13:37:10 +0300

    Add book user and book repositories in the repositories package
    - create an interface labelled UserRepository that extends the JpaRepository
    - create an interface labelled BookRepository that extends the JpaRepository
    - create 2 entities labelled BookEntity and UserEntity, the code inside will be altered once we're ready with the DTO classes.
    - comments have been added to fully understand better what the repositories do in the app.

---
+++ online-bookstore/src/main/java/group1/itschool/onlinebookstore/models/entities/BookEntity.java
@@ -0,0 +1,9 @@
+package group1.itschool.onlinebookstore.models.entities;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Entity
+@Data
+public class BookEntity {
+}
--- online-bookstore/src/main/java/group1/itschool/onlinebookstore/models/entities/Test1.java
+++
@@ -1,4 +0,0 @@
-package group1.itschool.onlinebookstore.models.entities;
-
-public class Test1 {
-}
