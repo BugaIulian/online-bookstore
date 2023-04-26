8323b369872b86b386e0fd0a53c254af4ce6df8d
Razvan <razvantrofin743@yahoo.com>
2023-04-26 13:07:04 +0300

    - I created the properties for UserEntity

--- online-bookstore/src/main/java/group1/itschool/onlinebookstore/models/entities/UserEntity.java
+++ online-bookstore/src/main/java/group1/itschool/onlinebookstore/models/entities/UserEntity.java
@@ -2,15 +2,28 @@ package group1.itschool.onlinebookstore.models.entities;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 
-//@Data
-//@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-//@Entity
-//@Table(name = "users")
-//public class UserEntity {
-//
-//}
+@Data
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Column(name = "username")
+    private String username;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "fullName")
+    private String fullName;
+    @Column(name = "dateOfBirth")
+    private String dateOfBirth;
+    @Column(name = "interests")
+    private String interests;
+    @Column(name = "accountCreatinDate")
+    private String accountCreationDate;
+}
